52cec87398bad4fae3d2c4579af1c1e872df5bb2
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2020-07-15 22:24:57 -0300

    Criando pasta static para imagens

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import uploadConfig from './config/upload'
 
 import cors from 'cors';
 import routes from './routes';
 
 const app = express();
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory))
 app.use(cors());
 app.use(routes);
 
